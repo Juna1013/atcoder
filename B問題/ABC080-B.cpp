5016013de8a187ee6767f886a6b84c27b5b0fa8c
Junya <160022550+Juna1013@users.noreply.github.com>
2024-07-16 08:14:41 +0900

    Create ABC080-B.cpp

---
+++ B問題/ABC080-B.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int N;
+  cin >> N;
+ 
+  int fx = 0;
+  int x = N;
+  while (x) {
+    fx += x % 10;
+    x /= 10;
+    }
+ 
+  if (N % fx == 0) {
+    cout << "Yes" << endl;
+  }
+  else {
+    cout << "No" << endl;
+  }
+}
