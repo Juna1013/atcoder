d4bd65590ba434b1b986dafeb2c4615aa567c493
Junya <160022550+Juna1013@users.noreply.github.com>
2025-01-05 10:42:55 +0900

    Delete ABC directory

--- ABC/A問題/002-A.py
+++
@@ -1,7 +0,0 @@
-a,b = map(int,input().split())
-if a > b:
-   print(a)
-elif a == b:
-   print(b)
-else:
-   print(b)
--- ABC/A問題/008-A.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-  int s, t;
-  cin >> s >> t;
-  cout << t - s + 1 << endl;
-}
